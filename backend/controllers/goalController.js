284e234390ec7e6cfe037ba0279956b76422666b
JuniorBrPr <junior.britoperez@student.hu.nl>
2022-02-17 13:53:48 +0100

    Initial REST API for goalsap

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 //@desc     Get goals
 //@route    GET /api/goals
 //@access   Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.json({message: 'Get goals'})
+    const goals = await Goal.find()
+    res.json(goals)
 })
 
 //@desc     Set goal
@@ -16,21 +19,43 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.json({message: 'Set goal'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 //@desc     Update goal
 //@route    PUT /api/goals/:id
 //@access   Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.json(updatedGoal)
 })
 
 //@desc     Delete goal
 //@route    DELETE /api/goals/:id
 //@access   Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.json({message: `Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.json({id : req.params.id})
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const colors = require("colors")
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+console.log(process.env.MONGO_URI)
+
+connectDB()
 
 const app = express()
 
